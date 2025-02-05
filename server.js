e2b72a6e7cdc26fc12cdf6abfb0b2641ce8dc60d
OsamaSafwan <osamasafwan5841366@gmail.com>
2025-02-05 15:56:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-app.listen(Port, () => {
+app.listen(Port || process.env.PORT, () => {
   console.log(`Server is running on port: ${Port}`);
 });
